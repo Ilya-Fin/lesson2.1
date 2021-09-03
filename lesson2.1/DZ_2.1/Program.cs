0e145308b55d99225196f0020ef3a0895dac0804
Ilya_TS <ilya.s.t@mail.ru>
2021-09-03 17:10:57 +0300

    Added lesson2.(1.2.3.4.5.6)

---
+++ lesson2.1/DZ_2.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DZ_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите МИНИМАЛЬНУЮ температуру за сутки");
+            string a = Console.ReadLine();
+            Console.WriteLine("Введите МАКСИМАЛЬНУЮ температуру за сутки");
+            string b = Console.ReadLine();
+            Console.WriteLine($"Среднесуточная температура {(Convert.ToDecimal(a) + Convert.ToDecimal(b)) / 2}");
+            Console.ReadKey();
+        }
+    }
+}
